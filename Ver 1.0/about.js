a494f21ccea011c430030a5f8e1d8be9e5018969
Roberto Jr. <115932135+Paoseu@users.noreply.github.com>
2025-01-15 22:52:31 +0800

    Update about.js

--- Ver 1.0/about.js
+++ Ver 1.0/about.js
@@ -9,3 +9,17 @@ menuBtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
     sideMenu.style.display = 'none';
 })
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
